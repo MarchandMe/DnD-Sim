df44d2df5dc816412dff799c7378c3657d2a30bf
Lawrence <nospamformarch@gmail.com>
2025-04-24 10:16:39 +0800

    added dice

--- scenes/testscript.gd
+++ scenes/testscript.gd
@@ -21,9 +21,40 @@ func get_stats(chardat: Dictionary):
 func calc_ability_modifiers(chardat: Dictionary):
 	var stats = get_stats(chardat)
 	var bonuses = {}
-	
+	bonuses["strength"] = int(stats["strength"]/2) - 5
+	bonuses["dexterity"] = int(stats["dexterity"]/2) - 5
+	bonuses["constitution"] = int(stats["constitution"]/2) - 5
+	bonuses["intelligence"] = int(stats["intelligence"]/2) - 5
+	bonuses["wisdom"] = int(stats["wisdom"]/2) - 5
+	bonuses["charisma"] = int(stats["charisma"]/2) - 5
+	return bonuses
+
+func roll_single_dice(lb: int, ub:int): #upper and lower bounds, inclusive
+	return randi()%(ub-lb+1) + lb
+
+#common dice macros
+func d20():
+	return roll_single_dice(1,20)
+func d12():
+	return roll_single_dice(1,12)
+func d10():
+	return roll_single_dice(1,10)
+func d6():
+	return roll_single_dice(1,6)
+func d4():
+	return roll_single_dice(1,4)
+func d3():
+	return roll_single_dice(1,3)
+
+func roll_multiple_dice(dicestr: String):
+	return 0
 
 func _ready() -> void:
+	randomize() # init randomizer
 	#print(load_from_file("res://data/player data/playerchartemplatetest.json"))
 	var chardat = load_json("res://data/player data/playerchartemplatetest.json")
 	print(get_stats(chardat))
+	var abm = calc_ability_modifiers(chardat)
+	print(abm)
+	print(d3())
+	
